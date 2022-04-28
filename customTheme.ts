c57aeca92329715ae8b6a3e06842327cf352eaaf
Kuchukov Dmitry <kudim@inbox.ru>
2022-04-28 11:42:19 +0300

    add fonts Nunito and Open Sans

---
+++ customTheme.ts
@@ -0,0 +1,10 @@
+import {extendTheme} from '@chakra-ui/react'
+
+const customTheme = extendTheme({
+    fonts: {
+        heading: 'Nunito, sans-serif',
+        body: 'Open Sans, sans-serif',
+    },
+})
+
+export default customTheme

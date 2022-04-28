94fdf8b46fcfbad1ca06009ec3f0824fd02a8673
Kuchukov Dmitry <kudim@inbox.ru>
2022-04-28 11:13:18 +0300

    create project Nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['beauty-schools.ru']
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig

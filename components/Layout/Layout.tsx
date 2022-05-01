aa9bf98e001e861d31b50312e50832124da7c5a1
Kuchukov Dmitry <kudim@inbox.ru>
2022-05-01 10:27:19 +0300

    add MainMenu in Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import {ILayoutProps} from './ILayout.props'
 import styles from './Layout.module.scss'
 import HeaderTop from './HeaderTop/HeaderTop'
+import MainMenu from './MainMenu/MainMenu'
+import {pages} from '../../mock/pages'
 
 const Layout = ({...props}: ILayoutProps): JSX.Element => {
     return (
-        <HeaderTop/>
+        <>
+            <HeaderTop />
+            <MainMenu pages={pages}/>
+        </>
     )
 }
 

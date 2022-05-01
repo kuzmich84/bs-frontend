a2a6acafe07bc47699e3d0ffb8bb527d37b891ca
Kuchukov Dmitry <kudim@inbox.ru>
2022-05-01 10:23:36 +0300

    add mocks

---
+++ mock/pages.ts
@@ -0,0 +1,21 @@
+export const pages = [
+    {
+        name: 'Главная',
+        url: '/',
+    },
+    {
+        name: 'Курсы',
+        url: '/courses',
+    },
+    {
+        name: 'События',
+        url: '/events',
+    },
+    {
+        name: 'Блог',
+        url: '/blog',
+    },
+    {
+        name: 'Контакты',
+        url: '/contacts',
+    }]
---
+++ pages/courses.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type {NextPage} from 'next'
+
+const Courses = () => {
+    return (
+        <div>
+            Page Courses
+        </div>
+    )
+}
+
+export default Courses

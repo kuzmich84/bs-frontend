2ceef068b91df534614cb5f87788d21fad6fef36
Kuchukov Dmitry <kudim@inbox.ru>
2022-06-11 23:51:16 +0300

    add upload to S3 VK method with multer-s3

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,9 +1,10 @@
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import nc from 'next-connect'
 import * as uuid from 'uuid'
-import {S3Client, AbortMultipartUploadCommand, CreateBucketCommand, PutObjectCommand} from '@aws-sdk/client-s3'
-import path from 'path'
-import {NextApiRequest, NextApiResponse} from 'next'
-import formidable from 'formidable'
-import * as fs from 'fs'
+import type {NextApiRequest, NextApiResponse} from 'next'
+import client from '../../lib/aws-config'
+
 
 export const config = {
     api: {
@@ -11,73 +12,30 @@ export const config = {
     },
 }
 
-const client = new S3Client({
-    credentials: {
-        accessKeyId: '201867_Dmitry',
-        secretAccessKey: 'Kudim1984!',
-    },
-    endpoint: 'https://s3.storage.selcloud.ru',
-    region: 'ru-1',
-    apiVersion: 'latest',
+const upload = multer({
+    storage: multerS3({
+        s3: client,
+        bucket: process.env.AWS_BUCKET_NAME!,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname})
+        },
+        key: function (req, file, cb) {
+            cb(null, `user_${req.body.userid}/${uuid.v4()}.jpg`)
+        },
+    }),
+
 })
 
+const handler = nc<NextApiRequest, NextApiResponse>()
+    .use(upload.single('picture'))
+    .post((req, res) => {
+        return res.json({hello: 'file upload', url: req.file.location})
+    })
+    .patch(async (req, res) => {
+        throw new Error('Throws me around! Error can be caught and handled.')
+    })
 
-const params = {
-    Bucket: 'bs-data',
-}
-
-
-const saveFile = async (file) => {
-    try {
-        const image = fs.readFileSync(file.filepath)
-        const fileName = file.newFilename
-        fs.writeFileSync(path.resolve('public', fileName + '.jpg'), image)
-        await fs.unlinkSync(file.filepath)
-
-        const data = await client.send(new PutObjectCommand({...params, Key: `test/${fileName}`, Body: fs.createReadStream(file.filepath)}))
-
-        // const filePath = path.resolve('public', fileName)
-        // console.log(fileName)
-        // await file.mv(filePath)
-        console.log(data)
-        return
-    } catch (e) {
-        console.error(e)
-    }
-}
-
-export default async function upload(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        // const form = formidable({ multiples: true });
-        //
-        // form.parse(req, async (err, fields, files) => {
-        //     if (err) {
-        //         res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
-        //         res.end(String(err));
-        //         return;
-        //     }
-        //     console.log(files.picture)
-        //      await saveFile(files.picture)
-        //
-        // });
-        console.log(req)
-
-        return res.status(200).json({message: 'upload'})
-    }
-
-    // form.parse(req, (err, fields, files) => {
-    //     if (err) {
-    //         res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
-    //         res.end(String(err));
-    //         return;
-    //     }
-    //     res.writeHead(200, { 'Content-Type': 'application/json' });
-    //     res.end(JSON.stringify({ fields, files }, null, 2));
-    // });
-    //
-    // return;
-
-
-    return res.status(403).send('Forbidden method')
-}
+export default handler
 
